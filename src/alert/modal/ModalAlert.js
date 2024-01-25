1f448aad5043099781bd46c2296104a43613771e
Radoslaw Sawicki <48261599+Sawikr@users.noreply.github.com>
2024-01-25 22:21:41 +0100

    Notes: updating a modal alert - adding more information

--- src/alert/modal/ModalAlert.js
+++ src/alert/modal/ModalAlert.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
 import {
     MDBBtn,
     MDBModal,
@@ -34,7 +35,14 @@ export default function ModalAlert() {
                             <MDBModalTitle>Note</MDBModalTitle>
                         </MDBModalHeader>
                         <MDBModalBody>
-                            As of January 2024, the Replit platform has changed its hosting policies. The repl.co domains have been moved to replit.dev and will only be available when using the editor. This means that the server does not start automatically after logging in to the application.
+                            <p className="mt-2" style={{textAlign: "justify"}}>
+                                As of January 2024, the Replit platform has changed its hosting policies. The repl.co domains have been moved to replit.dev and will only be available when using the editor. This means that the server does not start automatically after logging in to the application.
+                            </p>
+                            <p style={{marginBottom: 10, textAlign: "justify"}}>
+                                For this reason, NotesApp is currently hosted on the Heroku platform. For more information about logging in to application, visit the NotesApp
+                                <Link to="https://github.com/Sawikr/radoslaw-sawicki-frontend-react-notesapp" className="primary-color" title='NotesApp repository'
+                                > repository</Link>.
+                            </p>
                         </MDBModalBody>
                         <MDBModalFooter>
                             <MDBBtn className='button-modal' color='none' onClick={toggleClose}>
